0b3de49acb3a8f2b89b935e878b76a8aa5c20c70
Movieeesss <114383509+Movieeesss@users.noreply.github.com>
2026-04-22 23:58:38 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SiteReport from './WOODESTIMATE' // Import the new component
+import WoodEstimate from './WOODESTIMATE'
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SiteReport /> {/* Render the Site Report Builder */}
+    <WoodEstimate />
   </React.StrictMode>,
 )
